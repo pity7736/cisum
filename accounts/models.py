3549231f530c93ae3ae39360a901fa00c7482b5d
Julián <pity7736@gmail.com>
2017-04-06 22:27:22 -0500

    add get_full_name method

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+from accounts.managers import UserManager
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50, null=True, blank=True)
@@ -10,9 +12,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
+    objects = UserManager()
     USERNAME_FIELD = 'email'
 
     def __str__(self):
+        return self.get_full_name()
+
+    def get_full_name(self):
         if self.first_name and self.last_name:
             return '{} {}'.format(self.first_name, self.last_name)
         return self.email
--- accounts/tests/test_user_model.py
+++ accounts/tests/test_user_model.py
@@ -18,3 +18,14 @@ names = (
 def test_str_user_without_first_name_or_last_name(first_name, last_name):
     user = UserFactory.build(email='pity7736@gmail.com', first_name=first_name, last_name=last_name)
     assert user.__str__() == 'pity7736@gmail.com'
+
+
+def test_get_full_name():
+    user = UserFactory.build(first_name='julian', last_name='cortes')
+    assert user.get_full_name() == 'julian cortes'
+
+
+@mark.parametrize('first_name,last_name', names)
+def test_et_full_name_without_first_name_or_last_name(first_name, last_name):
+    user = UserFactory.build(email='pity7736@gmail.com', first_name=first_name, last_name=last_name)
+    assert user.get_full_name() == 'pity7736@gmail.com'
